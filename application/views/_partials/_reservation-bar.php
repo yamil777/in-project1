62953e309a98cb7838f057a763b17aece1395dd9
Luciano Jorge <luciano.x.jorge.-nd@disney.com>
2015-06-22 13:10:45 -0300

    styles on reservation bar

--- application/views/_partials/_reservation-bar.php
+++ application/views/_partials/_reservation-bar.php
@@ -1,15 +1,15 @@
-	<div class="reservation-bar-container carousel-indicators col-xs-12">
-		<div class="col-xs-3">
+	<div class="row reservation-bar-container carousel-indicators col-xs-12">
+		<div class="col-xs-3 reservation-tile">
 			<label>LLEGADA</label>
 			<span class="font-icon">&#xe801;</span>
 		</div>
-		<div class="col-xs-3">
+		<div class="col-xs-3 reservation-tile">
 			<label>SALIDA</label>
 		</div>
-		<div class="col-xs-3">
+		<div class="col-xs-3 reservation-tile">
 			<label>PERSONAS</label>
 		</div>
-		<div class="col-xs-3">
+		<div class="col-xs-3 reservation-tile">
 			<button class="btn btn-default btn-lg btn-reservar">
 				RESERVAR
 			</button>
