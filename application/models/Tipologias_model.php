17d152c5612e26e377baf875ecfd35989288d95a
Luciano Jorge <yamil777@gmail.com>
2015-08-21 19:31:57 -0300

    adding database connection, and tipologias should be show for persons

--- application/models/Tipologias_model.php
+++ application/models/Tipologias_model.php
@@ -1,70 +1,30 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 class Tipologias_model extends CI_Model {
+
     public function __construct() {
         parent::__construct();
     }
 
     public function getTipologias($config) {
-    	$validApartments = $this->getMockData();
-
-    	/*$validApartments[0] = $apartment[0];
-    	$validApartments[1] = $apartment[1]; */
-    	return $validApartments;
-    	if(empty($config)) {
-    	}
-    }
-
-    private function getMockData() {
-		$apartment[0] = array(
-				'id' => 1,
-				'type' => 'Basico dos personas',
-				'description' => 'Departamento para dos personas, con cama matrimonial, incluye...'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.',
-				'estado' => '1'
-			);
-		$apartment[1] = array(
-				'id' => 1,
-				'type' => 'Completo dos personas',
-				'description' => 'Departamento para dos personas, con cama matrimonial, incluye...'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.',
-				'estado' => '1'
-			);
-		$apartment[2] = array(
-				'id' => 1,
-				'type' => 'Basico cuatro personas',
-				'description' => 'Departamento para cuatro personas, con cama matrimonial, incluye...'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.',
-				'estado' => '1'
-			);
-		$apartment[3] = array(
-				'id' => 1,
-				'type' => 'Completo cuatro personas',
-				'description' => 'Departamento para cuatro personas, con cama matrimonial, incluye...'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.'
-				.'sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.sarasa.',
-				'estado' => '1'
-			);
-
-		return $apartment;
+    	$validApartments = array();
+    	$this->load->database();
+        if(isset($config) 
+            && count($config) > 0 
+            && $config["persons"] > 0) {
+            
+            $this->db->where('max_capacity_tp >=', $config["persons"]);
+        
+        } 
+        
+    	$query = $this->db->get('tipologias');
+        /*
+    	foreach ($query->result() as $row)
+		{
+		    $validApartments[] = $row;
+		}
+        */
+    	return $query->result();
     }
 }
--- application/views/tipologias/tipologias.php
+++ application/views/tipologias/tipologias.php
@@ -1,5 +1,21 @@
 <?php
 	$this->load->view('_partials/layout/_header');
 	$this->load->view('_partials/_menu');
+	/*
+	echo "<pre>";
 	var_dump($availableApartments);
+	die;
+	*/
+	?>
+	<div class="container-tipologias">
+		<?php 
+		foreach($availableApartments as $key=>$val) : ?>
+			<div class="tipologia-min <?=$key > 1 ? 'down' : 'top';?>" id="<?=$val->id_tp;?>" position="<?=$key;?>">
+			</div>
+		<?php
+		endforeach;
+		?>
+		
+	</div>
+	<?php
 	$this->load->view('_partials/layout/_footer');
